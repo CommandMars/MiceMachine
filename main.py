cc9cb442fe22da14ad6a92a03aeb7fc9bcf6528f
CommandMars <106407289+CommandMars@users.noreply.github.com>
2023-10-17 21:32:49 +0000

    i'm addicted and i just can't get enough
    changed some orders, made errors (if they appear) more clearly, improved code readability, other small fixed i've already forgot i made

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import csv
 #some more crazy stuff noone will really need
 def exit(reason):
   print(reason)
-  sys.exit()
+  sys.exit("\n\n" + reason)
 
 
 print("Mäuserechner\nBerechne, wie viele Mäuse nach einer bestimmten Anzahl an Zyklen leben würden.")
@@ -23,8 +23,8 @@ try:
   boomers = int(input("\nAnzahl der erwachsenen Tiere: \n"))
   old = int(input("\nAnzahl der alten Tiere: \n"))
   cycles = int(input("\nAnzahl der Zyklen: \n"))
-except:
-  exit("\n\nNope.")
+except ValueError:
+  exit("Nope.")
 
 
 print("\nSollen die einzelnen Werte verändert werden?")
@@ -36,25 +36,26 @@ if alterEverything == "y":
   boomersToOld = int(input("erwachsene Tiere zu alten Tieren: \n"))
   decimalPlaces = int(input("Nachkommastellen: \n"))
 elif alterEverything == "n":
-  print("Normaleinstellungen werden übernommen.")
+  print("Normaleinstellungen werden übernommen. \n(4/2/2/3/0)")
   boomersToYouth = 4
   oldToYouth = 2
   youthToBoomers = 2
   boomersToOld = 3
   decimalPlaces = 0
 else:
-  exit("\n\nStop it. Get some help.")
+  exit("Stop it. Get some help.")
 
 
 print("\nSoll die Anzahl der Tiere nach jedem Zyklus angezeigt werden?")
 showAnimalsAfterEveryCycle = input("y/n\n")
 try:
   
-  if showAnimalsAfterEveryCycle != ("y" or "n"):
+  if showAnimalsAfterEveryCycle == "y" or "n":
   
     try:
       
       startSimulation = input("\nMit Enter die Simulation starten.")
+      
       if startSimulation == "":
         #open or create the csv file and run the mice simulation
         with open("table.csv", "w") as csvfile:
@@ -65,16 +66,15 @@ try:
           for i in range(cycles):
             youthTemporarily = youth
             boomersTemporarily = boomers
-            youth = round(((boomers * boomersToYouth) + (old * oldToYouth)),decimalPlaces)
-            boomers = round((youthTemporarily / youthToBoomers), decimalPlaces)
-            old = round((boomersTemporarily / boomersToOld), decimalPlaces)
+            youth = round( ( ( boomers * boomersToYouth ) + ( old * oldToYouth ) ) , decimalPlaces )
+            boomers = round( ( youthTemporarily / youthToBoomers ) , decimalPlaces )
+            old = round( ( boomersTemporarily / boomersToOld ) , decimalPlaces )
             try:
               writer.writerow([i + 1, youth, boomers, old])
-            except:
-              exit("Zyklus " + str(i+1) + " fehlgeschlagen.")
-            else:
               if (i+1) % 10 == 0:
                 print("Zyklus " + str(i+1) + " erfolgreich")
+            except:
+              exit("Zyklus " + str(i+1) + " fehlgeschlagen.")
           
         
             #if the user wants to, it is possible to see the current status after every successful cycle
@@ -84,19 +84,24 @@ try:
               print("Erwachsene Tiere: " + str(boomers))
               print("Alte Tiere: " + str(old))
               print("Insgesamt " + str(youth + boomers + old) + " Tiere.")
+    except ValueError:
+      exit("PRESS ENTER I SAID!")
     except:
-      exit("\n\nPRESS ENTER I SAID!")
+      exit("I don't know, just don't do the same thing you did before. CHANGE IT!")
 
+    print("\nSimulation beendet.")
+    
 except:
   exit("WHY? just WHY?")
   
 #a review
 if showAnimalsAfterEveryCycle == "n":
-  print("\nNach " + str(cycles) + " Zyklen:")
+  print("\nErgebnis:")
+  print("Nach " + str(cycles) + " Zyklen:")
   print("Anzahl der jungen Tiere: " + str(youth))
   print("Anzahl der erwachsenen Tiere: " + str(boomers))
   print("Anzahl der alten Tiere: " + str(old))
 
-print("\nSimulation beendet.")
 
+print("\n\n\n\n\nthe end")
 #the end
